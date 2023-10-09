99ad4eb23b592d93d155d263d32d42662a680919
Ethan Travasso <85579428+SkellXC@users.noreply.github.com>
2023-10-09 11:23:59 +0100

    Update main.py
    The player can now move around the board and gain coins when completing the challenge. Bombs take away lives and when lives hit zero, the game ends.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from math import e
 from random import randint
+from turtle import pos
 board = [[0 for x in range(10)] for x in range(10)]
 # Creates a 10 by 10 array full of zeros using list comprehension
 
@@ -9,6 +11,7 @@ def add_bombs(board):
         #print(x,y)
         board[y][x] = 1 # sets it to 1 so that when the player is on 1, the game knows a bomb is in that spot
     board[0][0] = 0# spawn point can't have a bomb
+    board[9][9] = 0# end point can't have a bomb
     return board
 
 def showboard(board):
@@ -26,38 +29,71 @@ class player:
         return [self.position[0],self.position[1]]
     def lose_life(self):
         self.lives -= 1# Take away lives from the player
-        print(f"Uh oh! You hit a bomb and lost a life!\nYou now have {f'{self.lives} lives left' if self.lives > 1 else '1 life left'}")
+        print(f"Uh oh! You hit a bomb and lost a life!\nYou now have {f'{self.lives} lives left' if self.lives != 1 else f'{self.lives} life left'}")
         if self.lives == 0:# check if the player should be dead
+            #print(self.lives)
             self.dead()
             
         
     def dead(self):
         print("You just died! Game Over.")
+        exit()
+        
         # Will add more stuff here sometime soon
+
     def traverse(self,position,move):# Coords in the form y,x
         if move == "l":# left
-            #board[pos[0]] -= 1
-            pass
+            if position[1] == 0:
+                print("You can't go any further left")
+            else:
+                self.position[1] -= 1
+                print(f"Moved successfully. You are now at {self.position} (y,x)")
+            
+            
         elif move == "r":# right
-            #board[pos[0]] += 1
-            pass
+            if position[1] == 9:
+                print("You can't go any further right")
+            else:
+                self.position[1] += 1
+                print(f"Moved successfully. You are now at {self.position} (y,x)")
+            
         elif move == "u":# up
-            self.position[1] -= 1
+            if position[0] == 0:
+                print("You can't go any further up")
+            else:
+                self.position[0] -= 1
+                print(f"Moved successfully. You are now at {self.position} (y,x)")
+
         elif move == "d":# down
-            self.position[0] += 1
+             if position[0] == 9:
+                print("You can't go any further down")
+             else:
+                self.position[0] += 1
+                print(f"Moved successfully. You are now at {self.position} (y,x)")
+
         else:
             print("This is not a valid move")
-            x -= 1
-            move = input("Which direction do you want to move in? Use U/D/L/R").lower()
+            #move = input("Use U/D/L/R\n").lower()
         if board[self.position[0]][self.position[1]] == 1:
             self.lose_life()
-        else:
-            print(f"Moved successfully. You are now at {self.position} (y,x)")
+        if position[0] == 9 and position[1] == 9:
+            self.coins += 10
+            print(f"You have reached the end! You now have {self.coins} coins!")
+            exit()
+
 
 add_bombs(board)
 showboard(board)
 
 
-me = player(2,10,[0,0])
-me.lose_life()
-me.traverse(me.position,"d")
+me = player(1,10,[0,0])
+#me.lose_life()
+me.traverse(me.position,"u")
+def play():
+   print("Which direction do you want to move in? Use U/D/L/R")
+   while True:
+        move = input("").lower()
+        me.traverse(me.position,move)
+
+            
+play()
